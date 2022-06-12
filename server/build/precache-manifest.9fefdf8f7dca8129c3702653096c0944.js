5447d9900cb9330ab59589cd2f05fca4355a5cc3
Dhananjay Baghdane <dhananjayvb11@gmail.com>
2022-06-12 21:59:03 +0530

    made some changes

---
+++ server/build/precache-manifest.9fefdf8f7dca8129c3702653096c0944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed37bc05efd79af787c421d92008ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73845d80bfea2b6cc6e7",
+    "url": "/static/css/main.41a8e922.chunk.css"
+  },
+  {
+    "revision": "a0866d7b46dab8967b57",
+    "url": "/static/js/2.bf05af71.chunk.js"
+  },
+  {
+    "revision": "440592a8cd6ffe2f27cce65699b296c9",
+    "url": "/static/js/2.bf05af71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73845d80bfea2b6cc6e7",
+    "url": "/static/js/main.e41cffd3.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "dccd3fc1631afd7d225dabf27bfea9fa",
+    "url": "/static/media/loading.dccd3fc1.svg"
+  }
+]);
